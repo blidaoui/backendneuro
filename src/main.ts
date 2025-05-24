5d168f6e91d3d2771b73dc9965ccc73493555278
blidaoui ibtihel <ibtihel.blidaoui@isimg.tn>
2025-05-24 18:16:08 +0100

    change

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('neuroBackend')
+  app.setGlobalPrefix('neuroBackend');
   app.enableCors({
-     origin: 'http://localhost:3000', // Adresse du frontend
-    credentials:true},
-  )
+origin: [ 'http://localhost:3000', 'https://neuroflowconsulting.com' ],
+    credentials: true,
+  });
 
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
