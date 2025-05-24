16dcb190a98ad83a861309e831986a60363f0752
blidaoui ibtihel <ibtihel.blidaoui@isimg.tn>
2025-05-24 18:02:10 +0100

    newchanges

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -2,17 +2,23 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        service: 'gmail',
-        auth: {
-          user: 'blidaouiibtihel22@gmail.com',
-          pass: 'nfmofxkvmqmbdlcb',
+    ConfigModule.forRoot(), // Charge les variables d'environnement
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+          auth: {
+            user: configService.get<string>('EMAIL_USER'),
+            pass: configService.get<string>('EMAIL_PASS'),
+          },
         },
-      },
+      }),
     }),
   ],
   controllers: [EmailController],
