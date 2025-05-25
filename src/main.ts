00d83be1edeebac2c212c01aa66837d45092df64
blidaoui ibtihel <ibtihel.blidaoui@isimg.tn>
2025-05-25 08:59:57 +0100

    add www to main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('neuroBackend');
   app.enableCors({
-  origin: [ 'https://neuroflowconsulting.com' ],
+origin: [ 'http://localhost:3000', 'https://www.neuroflowconsulting.com' ],
     credentials: true,
   });
 
